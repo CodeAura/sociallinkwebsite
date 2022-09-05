b30bf60eb2f0953037c0dafcdd6f180f3f004755
ValidCoder <mikedh.prive@gmail.com>
2022-09-05 16:32:34 +0200

    Fixed links

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 new Typed('.input', {
-    strings: ["Web Developer.", "Gamer.", "Developer.", "Photographer.", "Designer.", "Gym Guy."],
+    strings: ["Web Developer.", "Gamer.", "Professional Idiot", "Developer.", "Photographer.", "Designer.", "Gym Guy.", "PC Setup Geek"],
     typeSpeed: 110,
     backSpeed: 75,
     loop:true,
