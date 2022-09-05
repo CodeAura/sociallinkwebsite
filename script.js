2f54ec3e2eb8dfd0a8676a1ad33803de59c70b86
ValidCoder <mikedh.prive@gmail.com>
2022-09-05 15:53:09 +0200

    Done

---
+++ script.js
@@ -0,0 +1,14 @@
+new Typed('.input', {
+    strings: ["Web Developer.", "Gamer.", "Developer.", "Photographer.", "Designer.", "Gym Guy."],
+    typeSpeed: 110,
+    backSpeed: 75,
+    loop:true,
+});
+
+new Typed('.input1', {
+    strings: ["1364HP", "Mikelol909", "Velg", "CodeAura",],
+    typeSpeed: 110,
+    backSpeed: 75,
+    loop:true,
+});
+
